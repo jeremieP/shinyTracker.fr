d121fa0dbb33bd842687f30dfb249653deadc082
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2023-02-09 23:42:54 +0100

    Integrating the structure for the landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-// import { Inter } from '@next/font/google'
+import LogoLight from './logo-light.png'
+import Link from "next/link"
+import Twitch from './twitch.png'
+import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
 const inter = () => ({ className: '' })
 
 export default function Home() {
+
+  const today = new Date();
+  const year = today.getFullYear();
+
   return (
     <>
       <Head>
@@ -15,99 +22,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <main className="min-h-screen mx-52">
+        <section className='border-b flex justify-center py-12'>
+          <Image className="w-72 h-72" src={LogoLight} alt="logo shinytracker"/>
+        </section>
+        <section className='border-b flex flex-col justify-center align-center text-center py-12'>
+          <h2 className='text-4xl leading-10 font-bold mb-6'>Bientôt disponible</h2>
+          <p className='text-lg leading-7 font-normal'>Shinytracker, la plateforme qui vous permet de suivre vos pokémons shiny par génération ! Partez à la chasse aux shiny, et notez vos prises sur Shinytracker !</p>
+        </section>
+        <section className='border-b p-12 flex flex-col justify-center items-center'>
+          <div className='flex justify-between mb-16'>
+            <div>
+              <h3 className="text-xl leading-7 font-medium mb-6">Suivez l’évolution du projet en live</h3>
+              <p className='mb-6'>Regardez l’équipe travailler sur le projet, tous les jeudi soirs sur Twitch ! Les streams seront diffusés sur la chaine <Link className="text-red-500" href='https://www.twitch.tv/jeremiep_'>JeremieP_</Link>.</p>
+              <p>On vous fait découvrir Next.js en codant le projet. Venez poser vos questions, découvrir une nouvelle techno, ou discuter avec nous en stream !</p>
+            </div>
+            <Image src={Twitch} alt="logo twitch" className='w-52 h-52 ml-12'/>
           </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+          <Link className="bg-red-500 text-zinc-50 py-4 px-16 rounded-full max-w-fit text-center" href='https://github.com/jeremieP/shinyTracker.fr'>Suivez le projet</Link>
+        </section>
+        <footer className='p-12 flex justify-center'>
+          <p className="text-base leading-6 font-light">Made with love by <Link className="text-red-500" href='https://github.com/jeremieP'>JeremieP_</Link> & <Link className="text-red-500" href='https://github.com/krauzeure'>Krauzeure</Link> - © {year}</p>
+        </footer>
       </main>
     </>
   )
