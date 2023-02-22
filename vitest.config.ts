c5c9d9a45e6ff7c718049218f9b5823aa9681647
jeremieP <jeremie.pereira@gmail.com>
2023-02-22 22:08:27 +0100

    chore: add setup for vitest/TestingLibrary

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: './config/tests/setup.ts',
+    alias: {
+      '@': './src',
+    }
   },
 })
