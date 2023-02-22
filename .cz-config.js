1f17df8e0f191a7fd75dd747a99a4c0e2f42d7e3
jeremieP <jeremie.pereira@gmail.com>
2023-02-22 22:52:12 +0100

    chore: init commitizen

---
+++ .cz-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat: A new feature' },
+    { value: 'fix', name: 'fix: A bug fix' },
+    { value: 'docs', name: 'docs: Documentation only changes' },
+    { value: 'test', name: 'test: Adding missing tests' },
+    {
+  value: 'chore',
+  name: 'chore: Changes to the build process or auxiliary tools\n    and libraries such as documentation generation',
+    },
+    { value: 'revert', name: 'revert: Revert to a commit' },
+  ],
+  skipQuestions: ['footer', 'body'],
+  appendBranchNameToCommitMessage: false,
+  allowTicketNumber: false,
+}
