e3d3e818389a904522ddc57d55464f562cb672e2
jeremieP <jeremie.pereira@gmail.com>
2023-02-22 22:51:02 +0100

    feat: update Inter 'mock' on Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
-const inter = () => ''
+const inter = () => ({ className :''})
 
 export default function Home() {
   return (
